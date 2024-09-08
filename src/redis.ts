6d8e7befe1d33de2d24d90dd9ffe6d0d659e122f
Shon Abdullah <Abdullah@Skeno-Abdu-Rahman.local>
2024-09-08 05:16:14 -0500

    added console log-- debugging redis connection

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,7 @@
 import { createClient } from "redis";
 import env from "dotenv";
 env.config();
+console.log("REDIS_URL:", process.env.REDIS_URL); // Should print the Redis URL or undefined
 let redisClient = createClient({
   url: `redis://${process.env.REDIS_URL}`, // or whatever the server url is
 });
