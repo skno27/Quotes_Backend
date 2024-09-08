b70143aac3161e811bec501d89555f0caeb2528b
Shon Abdullah <Abdullah@Skeno-Abdu-Rahman.local>
2024-09-08 03:13:15 -0500

    added correct protocol to redis connection url

--- src/redis.ts
+++ src/redis.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 import env from "dotenv";
 env.config();
 let redisClient = createClient({
-  url: process.env.REDIS_URL, // or whatever the server url is
+  url: `redis://${process.env.REDIS_URL}`, // or whatever the server url is
 });
 
 export default redisClient;
